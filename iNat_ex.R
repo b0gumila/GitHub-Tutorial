89fcf2eb0b1fb909cf15cf10017b9f21e7b8f7b3
vkdearborn <vkdearborn@ucdavis.edu>
2021-04-26 13:40:55 -0700

    changed plot

--- iNat_ex.R
+++ iNat_ex.R
@@ -1,3 +1,5 @@
 library(lattice)
 inat<-read.csv("observations-145646.csv")
 xyplot(inat$latitude~inat$longitude,pch=16,groups=inat$taxon_class_name)
+
+xyplot(inat$latitude~inat$longitude,pch=16,groups=inat$common_name)
